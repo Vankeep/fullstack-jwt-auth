d7baf7cae19c684f9d421e3127fd67110174dd01
Vankeep <92756532+Vankeep@users.noreply.github.com>
2022-10-19 03:37:06 +0300

    Update index.ts

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -11,7 +11,7 @@ const $api = axios.create({
 })
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    config.headers = {Authorization:`Bearer ${localStorage.getItem('token')}`}
     return config;
 })
 
